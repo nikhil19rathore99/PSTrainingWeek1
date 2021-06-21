eb8dc33dc36251c79c8b3db4956ca4598f2bce3f
nikhil19rathore99 <nikhilrathoreb4i@gmail.com>
2021-06-21 16:45:49 +0530

    First Maven Project

--- projectadd/src/main/java/com/sapient/App.java
+++ projectadd/src/main/java/com/sapient/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! this is first maven project" );
         System.out.println(new App().add(3,2));
     }
     public int add(int a, int b){
