644e455c212955ef77b4bb14a82af0cf636477c3
nikhil19rathore99 <nikhilrathoreb4i@gmail.com>
2021-06-24 14:52:14 +0530

    Week 1 Day 3

---
+++ Week1/src/com/day3/sapient/Demo2.java
@@ -0,0 +1,8 @@
+package com.day3.sapient;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        FiguresToWords figuresToWords = new FiguresToWords();
+        System.out.println(figuresToWords.getFigures(1234l));
+    }
+}
